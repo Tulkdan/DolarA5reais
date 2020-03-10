9e91bafcbaabd4b410de8fd4b16bdd82b04b5b34
Pedro Correa <pedro.correa@dextra-sw.com>
2020-03-10 13:55:06 -0300

    Removed comments from template

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:is_dolar_five_reais/main.dart';
+import 'package:Dolar_a_cinco_reais/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
