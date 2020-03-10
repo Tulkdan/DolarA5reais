6ef5ce979a21c122acbbfe3b329429c435026824
Pedro Correa <pedro.correa@dextra-sw.com>
2020-03-10 13:50:00 -0300

    app commit

---
+++ android/app/src/main/kotlin/com/example/is_dolar_five_reais/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.is_dolar_five_reais
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
